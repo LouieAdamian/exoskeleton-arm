8a50310822b8032c964dcabc6f017af97811921b
Louie Adamian <LouieAdamian@users.noreply.github.com>
2017-05-23 14:38:23 -0400

    adds intial zeroing calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "arduino.h"
+#include <EEPROM.h>
 #include <RunningMedian.h>
 #define emgPin A0
 #define motorPin1 2
@@ -11,28 +12,38 @@ int lasterr;
 int integral;
 int derivative;
 int speed;
-float Kp;
-float Ki =0;
-float Kd =0;
+bool preset;
+float Kp ;
+float Ki ;
+float Kd ;
 float move;
-RunningMedian samples =RunningMedian(9);
+float newZero;
+RunningMedian samples = RunningMedian(5);
+RunningMedian samples2= RunningMedian(20);
 long emg;
+void tune(){
+for (int i = 0; i<20; i++){
+  int read = analogRead(A0);
+  samples2.add(read);
+}
+newZero = samples.getMedian();
+}
 void setup() {
   Serial.begin(115200);
   Serial.print("init");
+  preset = EEPROM.read(0);
+  tune();
 }
 void runMotor(int speed) {
 analogWrite(motorPin2, 0);
 analogWrite(motorPin1, speed);
 }
 void loop() {
-  // put your main code here, to run repeatedly:
   emg = analogRead(emgPin);
-  Serial.println(emg);
   samples.add(emg);
   long emgM = samples.getMedian();
   Serial.println(emgM);
-  err = offset - emg;
+  err = offset - emgM;
   derivative = err - lasterr;
   move = Kp * err + Ki * integral + Kd *derivative;
   runMotor(move);
