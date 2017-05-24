a08c7503cc7ba169f02f3370df7a9d91ff232321
Louie Adamian <LouieAdamian@users.noreply.github.com>
2017-05-24 10:50:46 -0400

    adds threshold

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,13 @@
 #define motorPin2 3
 #define encoderPinA 4
 #define encoderPinB 5
-int offset;
-int err;
-int lasterr;
-int integral;
-int derivative;
-int speed;
-bool preset;
-float Kp ;
-float Ki ;
-float Kd ;
-float move;
-float newZero;
+int offset, err, lastErr, integral, derivative, speed, preset, read, emg;
+float Kp, Ki,  Kd, move, newZero, emgA, lastemg;
 RunningMedian samples = RunningMedian(5);
-RunningMedian samples2= RunningMedian(20);
-long emg;
-void tune(){
-for (int i = 0; i<20; i++){
-  int read = analogRead(A0);
-  samples2.add(read);
-}
-newZero = samples.getMedian();
-}
 void setup() {
   Serial.begin(115200);
   Serial.print("init");
   preset = EEPROM.read(0);
-  tune();
 }
 void runMotor(int speed) {
 analogWrite(motorPin2, 0);
@@ -41,11 +21,14 @@ analogWrite(motorPin1, speed);
 void loop() {
   emg = analogRead(emgPin);
   samples.add(emg);
-  long emgM = samples.getMedian();
-  Serial.println(emgM);
-  err = offset - emgM;
-  derivative = err - lasterr;
+  emgA = samples.getAverage();
+  Serial.println(emgA);
+  if (lastemg - emgA > 150){
+  err = offset - emgA;
+  derivative = err - lastErr;
   move = Kp * err + Ki * integral + Kd *derivative;
   runMotor(move);
-  lasterr = err;
+  lastErr = err;
+  lastemg = emgA;
+}
 }
